5b99c1bb7d5dbde37bf6d137ec0ba81f50287658
berabyk <52321163+berabyk@users.noreply.github.com>
2022-05-29 23:52:46 +0300

    first commit

---
+++ Activity11_Slider/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  randomStart: true,
+			  pause: 3000,
+			  captions: true
+			});
+    	});
